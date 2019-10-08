b9a56ab93301e18245fffba5fc41db6fe00226c5
彭双塔 <arthur@soundbus.cn>
2019-10-08 16:53:10 +0800

    1、更新最新的两个广告平台：广点通sdk和穿山甲sdk

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 platform :ios,'9.0'
 target 'YX_AD_demo' do
-pod 'GDTMobSDK', '~> 4.8.10'
-pod 'Bytedance-UnionAD', '~> 1.9.8.5'
+pod 'GDTMobSDK', '~> 4.10.13'
+pod 'Bytedance-UnionAD', '~> 2.4.6.7'
 end
--- YX_AD_demo/ADCDN_SDK/ADCDN.framework/Headers/ADCDN_SplashAdManager.h
+++ YX_AD_demo/ADCDN_SDK/ADCDN.framework/Headers/ADCDN_SplashAdManager.h
@@ -43,7 +43,13 @@
 
 @property (nonatomic, strong)UIWindow * _Nullable window;
 @property (nonatomic, assign)CGRect wFrame;
-
+/**
+*  广告发起请求并展示在Window中, 同时在屏幕底部设置应用自身的Logo页面或是自定义View
+*  详解：[可选]发起拉取广告请求,并将获取的广告以半屏形式展示在传入的Window的上半部，剩余部分展示传入的bottomView
+*       请注意1.bottomView需设置好宽高，所占的空间不能过大，并保证高度不超过屏幕高度的 25%。2.Splash广告只支持竖屏
+* bottomView 自定义底部View，可以在此View中设置应用Logo
+*/
+@property (nonatomic, strong)UIView * _Nullable bottomView;
 
 @property (nonatomic, weak) id <ADCDN_SplashAdManagerDelegate> _Nullable delegate;
 /**
