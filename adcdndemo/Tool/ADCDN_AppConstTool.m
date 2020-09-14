f4410c76af8e46b6ff654c00e1e60a1ee84be90c
彭双塔 <1120160658@qq.com>
2020-09-14 17:23:34 +0800

    1、更新SDK，V3.8.0

--- adcdndemo/Tool/ADCDN_AppConstTool.m
+++ adcdndemo/Tool/ADCDN_AppConstTool.m
@@ -14,66 +14,66 @@
 * 广告位id
 */
 /** appId */
-NSString * const KappId = @"600001";
-/** 开屏广告 */
-NSString * const KplcId_Splash = @"2000006";
-/** 横幅广告 */
-NSString * const KplcId_Banner = @"2000017";
-/** 插屏广告 */
-NSString * const KplcId_Interstitial = @"2000014";
-/** 模版非激励视频 - 竖屏 */
-NSString * const KplcId_ExpressVideoVertical = @"2000010";
-/** 模版激励视频 - 横屏 */
-NSString * const KplcId_ExpressRewardVideoHorizon = @"0";
-/** 模版激励视频 - 竖屏 */
-NSString * const KplcId_ExpressRewardVideoVertical = @"2000019";
-/** 模版非激励视频 - 横屏 */
-NSString * const KplcId_ExpressVideoHorizon = @"2000095";
-/** 原生广告 - 三小图 */
-NSString * const KplcId_ThreeSmallImg = @"2000089";
-/** 原生广告 - 左图右文 */
-NSString * const KplcId_LeftImg = @"2000090";
-/** 原生广告 - 左文右图 */
-NSString * const KplcId_RightImg = @"2000091";
-/** 原生广告 - 文字浮层 */
-NSString * const KplcId_Text = @"2000092";
-/** 原生广告 - 上图下文 */
-NSString * const KplcId_TopImg = @"2000093";
-/** 原生广告 - 上文下浮层 */
-NSString * const KplcId_TopText = @"2000094";
+//NSString * const KappId = @"600001";
+///** 开屏广告 */
+//NSString * const KplcId_Splash = @"2000006";
+///** 横幅广告 */
+//NSString * const KplcId_Banner = @"2000017";
+///** 插屏广告 */
+//NSString * const KplcId_Interstitial = @"2000014";
+///** 模版非激励视频 - 竖屏 */
+//NSString * const KplcId_ExpressVideoVertical = @"2000010";
+///** 模版激励视频 - 横屏 */
+//NSString * const KplcId_ExpressRewardVideoHorizon = @"0";
+///** 模版激励视频 - 竖屏 */
+//NSString * const KplcId_ExpressRewardVideoVertical = @"2000019";
+///** 模版非激励视频 - 横屏 */
+//NSString * const KplcId_ExpressVideoHorizon = @"2000095";
+///** 原生广告 - 三小图 */
+//NSString * const KplcId_ThreeSmallImg = @"2000089";
+///** 原生广告 - 左图右文 */
+//NSString * const KplcId_LeftImg = @"2000090";
+///** 原生广告 - 左文右图 */
+//NSString * const KplcId_RightImg = @"2000091";
+///** 原生广告 - 文字浮层 */
+//NSString * const KplcId_Text = @"2000092";
+///** 原生广告 - 上图下文 */
+//NSString * const KplcId_TopImg = @"2000093";
+///** 原生广告 - 上文下浮层 */
+//NSString * const KplcId_TopText = @"2000094";
 
 
 /**
  * 记账精灵app正式环境的id
  */
 /** appId */
-//NSString * const KappId = @"600003";
-///** 开屏广告 */
-//NSString * const KplcId_Splash = @"2000024";
-///** 横幅广告 */
-//NSString * const KplcId_Banner = @"2000028";
-///** 插屏广告 */
-//NSString * const KplcId_Interstitial = @"2000027";
-///** 模版非激励视频 - 竖屏 */
-//NSString * const KplcId_ExpressVideoVertical = @"2000025";
-///** 模版激励视频 - 横屏 */
-//NSString * const KplcId_ExpressRewardVideoHorizon = @"1010458";
-///** 模版激励视频 - 竖屏 */
-//NSString * const KplcId_ExpressRewardVideoVertical = @"2000029";
-///** 模版非激励视频 - 横屏 */
-//NSString * const KplcId_ExpressVideoHorizon = @"1010461";
-///** 原生广告 - 三小图 */
-//NSString * const KplcId_ThreeSmallImg = @"2000026";
-///** 原生广告 - 左图右文 */
-//NSString * const KplcId_LeftImg = @"1010142";
-///** 原生广告 - 左文右图 */
-//NSString * const KplcId_RightImg = @"1010143";
-///** 原生广告 - 文字浮层 */
-//NSString * const KplcId_Text = @"1010140";
-///** 原生广告 - 上图下文 */
-//NSString * const KplcId_TopImg = @"1010145";
-///** 原生广告 - 上文下浮层 */
-//NSString * const KplcId_TopText = @"2000012";
+NSString * const KappId = @"600003";
+/** 开屏广告 */
+NSString * const KplcId_Splash = @"2000024";
+/** 横幅广告 */
+NSString * const KplcId_Banner = @"2000028";
+/** 插屏广告 */
+NSString * const KplcId_Interstitial = @"2000027";
+/** 模版非激励视频 - 竖屏 */
+NSString * const KplcId_ExpressVideoVertical = @"2000025";
+/** 模版激励视频 - 横屏 */
+NSString * const KplcId_ExpressRewardVideoHorizon = @"1010458";
+/** 模版激励视频 - 竖屏 */
+NSString * const KplcId_ExpressRewardVideoVertical = @"2000029";
+/** 模版非激励视频 - 横屏 */
+NSString * const KplcId_ExpressVideoHorizon = @"1010461";
+/** 原生广告 - 三小图 */
+NSString * const KplcId_ThreeSmallImg = @"2000026";
+/** 原生广告 - 左图右文 */
+NSString * const KplcId_LeftImg = @"1010142";
+/** 原生广告 - 左文右图 */
+NSString * const KplcId_RightImg = @"1010143";
+/** 原生广告 - 文字浮层 */
+NSString * const KplcId_Text = @"1010140";
+/** 原生广告 - 上图下文 */
+NSString * const KplcId_TopImg = @"1010145";
+/** 原生广告 - 上文下浮层 */
+NSString * const KplcId_TopText = @"2000012";
 
 
 @implementation ADCDN_AppConstTool
