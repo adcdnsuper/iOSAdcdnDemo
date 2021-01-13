de4125d493da94e8a3c9dccc76211171061f6a80
彭双塔 <1120160658@qq.com>
2021-01-13 10:56:49 +0800

    1、更新ADCDN V8.6.2,适配最新的穿山甲3.4.1.1和优良汇4.12.1版本。

--- Podfile
+++ Podfile
@@ -1,6 +1,8 @@
 platform :ios,'9.0'
 target 'adcdndemo' do
-#pod 'GDTMobSDK', '4.11.12'
-#pod 'Bytedance-UnionAD', '3.2.6.2'
+# 优良汇依赖包
+#pod 'GDTMobSDK', '4.12.1'
+# 穿山甲依赖包 注：穿山甲最新的包改名成Ads-CN,后续版本都在这个包下迭代
+#pod 'Ads-CN','3.4.1.1'
 end
 
--- Pods/Target Support Files/Pods-adcdndemo/Pods-adcdndemo-resources.sh
+++ Pods/Target Support Files/Pods-adcdndemo/Pods-adcdndemo-resources.sh
@@ -97,10 +97,10 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_ROOT}/Bytedance-UnionAD/Bytedance-UnionAd/Frameworks/BUAdSDK.bundle"
+  install_resource "${PODS_ROOT}/Ads-CN/PangleSDK/BUAdSDK.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_ROOT}/Bytedance-UnionAD/Bytedance-UnionAd/Frameworks/BUAdSDK.bundle"
+  install_resource "${PODS_ROOT}/Ads-CN/PangleSDK/BUAdSDK.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
